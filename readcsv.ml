8cb50e531cf94a98cc9f81dd8216f409343e3922
Ryan Mao <rwm275@cornell.edu>
2021-03-31 00:47:25 -0400

    fix condition in line concat

--- readcsv.ml
+++ readcsv.ml
@@ -5,18 +5,19 @@ type t = {
 
 let parseline str = failwith "Unimplemented"
 
+let odd_quotes str = failwith "Unimplemented"
+
 let readlines channel = 
   let read = ref [||] in
   try 
     while true do
       let line = ref (input_line channel) in 
       let newline = 
-        if String.get !line 0 == '"' && 
-           String.get !line ((String.length !line) - 1) <> '"' 
-      then (while String.get !line ((String.length !line) - 1) <> '"' do 
-        line := !line ^ input_line channel
-        done; !line)
-      else !line in
+        if odd_quotes line
+        then (while odd_quotes line do 
+                line := !line ^ input_line channel
+              done; !line)
+        else !line in
       read := Array.append !read (Array.make 1 (parseline newline))
     done; !read
   with 
