913135d913a0caf3c5a4157672b272d5b4ab91b7
pyuan <py95@cornell.edu>
2021-04-01 23:54:38 -0400

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,4 +1,5 @@
 open Readcsv
+open Command_parser
 
 let read_csv file = 
   print_endline
@@ -12,6 +13,15 @@ let read_csv file =
     match read_line () with 
     | exception End_of_file -> ()
     | file_name -> Readcsv.print_csv (Readcsv.from_csv file) file_name*)
+
+let read_command =   
+  print_endline
+  "Please enter the command you want to parse. We will print it to the terminal. \n";
+  match read_line () with 
+  | exception a -> ()
+  | com -> 
+    let q_tokens = Command_parser.tokenizer com in 
+    Command_parser.print_query_tokens q_tokens
 
 let main () = 
   ANSITerminal.print_string [ ANSITerminal.red ]
