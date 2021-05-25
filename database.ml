271dbef2a0f131340e3314b2c6f3d30aa62ba5f8
your boi <py95@cornell.edu>
2021-05-25 00:21:22 -0400

    bruh

--- database.ml
+++ database.ml
@@ -1,18 +1,18 @@
 open Table
 
-module Database = Map.Make(String)
+module Dad = Map.Make(String)
 
-type t = Table.t Database.t
+type t = Table.t Dad.t
 
-let empty_database () = Database.empty
+let empty_database () = Dad.empty
 
 let make_database table_names tables = 
   let d = ref (empty_database ()) in 
-  List.iter2 (fun a b -> d := Database.add a b !d) table_names tables;
+  List.iter2 (fun a b -> d := Dad.add a b !d) table_names tables;
   !d
 
 let get_table tblname db = 
-  Database.find tblname db 
+  Dad.find tblname db 
 
 let execute_query query db = 
   let query = Command_parser.parse_query query in 
