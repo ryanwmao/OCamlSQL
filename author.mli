6253db22114a6b6e231956d9d0ab6b35492d72ea
Ryan Mao <rwm275@cornell.edu>
2021-05-24 23:45:46 -0400

    documentation

--- author.mli
+++ author.mli
@@ -1,6 +1,5 @@
-(**
-    @author Ryan Mao (rwm275), Patrick Yuan (py95), Kelvin Wang (klw248) *)
+(** @author Ryan Mao (rwm275), Patrick Yuan (py95), Kelvin Wang (klw248) *)
 
-(** [hours_worked] is the number of hours each team member 
-    worked on this project. *)
+(** [hours_worked] is the number of hours each team member worked on
+    this project. *)
 val hours_worked : int list
--- eval.mli
+++ eval.mli
@@ -1,5 +1,8 @@
+(** Evaluation of queries *)
+
 open Ast
 open Database
 open Table
 
+(** Evaluates a query and returns the result as a table *)
 val evaluate_query : Ast.query -> Database.t -> Table.t
